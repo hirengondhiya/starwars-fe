2e2a13675ab4be76fe8dff3a0daa9a1f74325c51
Hiren Gondhiya <hirengondhiya@gmail.com>
2021-06-06 19:22:15 +1000

    people page slice

---
+++ src/store/peoplePageSlice.js
@@ -0,0 +1,61 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import swapiApi from "../api/swapiApi";
+
+export const fetchPeoplePageById = createAsyncThunk(
+  "peoplePage/fetchPeoplePageById",
+  async (params, thunkApi) => {
+    const resp = await swapiApi.get("people", { params });
+    return { ...params, ...resp.data };
+  },
+  {
+    condition: (arg, { getState }) => {
+      const page = getState().peoplePages.find(({ page }) => page === arg.page);
+      // cancel api request if page is in store or is being fetched
+      return !(
+        page &&
+        (page.requestStatus === "fulfilled" || page.requestStatus === "pending")
+      );
+    },
+  }
+);
+
+const initialState = [];
+
+export const peoplePageSlice = createSlice({
+  name: "peoplePage",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [fetchPeoplePageById.pending]: (state, action) => {
+      state.push({
+        ...action.meta.arg,
+        requestStatus: action.meta.requestStatus,
+      });
+    },
+    [fetchPeoplePageById.fulfilled]: (state, action) => {
+      const index = state.findIndex(
+        ({ page }) => action.meta.arg.page === page
+      );
+      if (index > -1) {
+        state[index] = {
+          ...action.payload,
+          requestStatus: action.meta.requestStatus,
+        };
+      }
+    },
+    [fetchPeoplePageById.rejected]: (state, action) => {
+      const index = state.findIndex(
+        ({ page }) => action.meta.arg.page === page
+      );
+      if (index > -1) {
+        state[index] = {
+          ...action.meta.arg,
+          requestStatus: action.meta.requestStatus,
+          errorMsg: action.error.message,
+        };
+      }
+    },
+  },
+});
+
+export default peoplePageSlice.reducer;
